840716eb50f64816e9fcd8c7cc3d9e8079a4c73d
nikolaivanovski <ivanovskinicola@gmail.com>
2023-01-18 00:00:26 +0100

    movies rent app

---
+++ Homework_MoviesRent/script.js
@@ -0,0 +1,47 @@
+console.log('test');
+// Movies renting App 
+// Create a movie renting app 
+// There should be an array of movie names 
+// There should be an input and a search button 
+// When the person enters a name of a movie it should search the array 
+// If the name exists it should show an H1 element that says: "The movie can be rented"
+// in green text If the name does not exist it should show an H1 element that says: 
+// "The movie can't be rented" in red text 
+// The input should not be case sensitive (it should find the movie regardless of capital or small letters )
+
+let movies = ['Nemo', 'Star Wars: Return of the Jedi', 'Avatar', 'Die Hard', 'Who Am I', 'Now You See Me'];
+
+let searchBar = document.getElementById('searchBar').value;
+let btn = document.querySelector('#btn');
+let result = document.querySelector('#result')
+let found = false;
+
+function findMovie() {
+    let announcement = document.createElement('h1');
+    for (let i=0; i < movies.length; i++) {
+        let lowerCaseTitle = movies[i].toLowerCase();
+        if(lowerCaseTitle === searchBar) {
+            console.log(movies[i]);
+            console.log(searchBar);
+            found = true;
+            break;
+        } else {
+            console.log(searchBar);
+            console.log('Movie not found');
+            break;
+        }
+    }
+    if (found) {
+        result.appendChild(announcement).innerHTML = 'The movie can be rented';
+        result.appendChild(announcement).style.color = 'green';
+    } else {
+        result.appendChild(announcement).style.color = 'red';
+        result.appendChild(announcement).innerHTML = 'The movie can`t be rented';
+    }
+}
+
+
+btn.addEventListener('click', function() {
+    findMovie();
+    searchBar.value = '';
+})
