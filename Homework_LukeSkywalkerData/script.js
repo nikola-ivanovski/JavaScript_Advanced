1f8bc1a9038ce9aaf26636afc03030695ddc0c63
nikola-ivanovski <ivanovskinicola@gmail.com>
2023-01-19 19:39:33 +0100

    table updates

--- Homework_LukeSkywalkerData/script.js
+++ Homework_LukeSkywalkerData/script.js
@@ -35,16 +35,44 @@ function manageData(data) {
     starWarsContainer.appendChild(table);
     lukeName.innerText = data.name;
 
+    table.setAttribute('border', 1);
+
     for (let i=0; i < 1; i++) {
-        const row = document.createElement('tr');
+        let row = document.createElement('tr');
+        let rowSecond = document.createElement('tr')
+        let heightHeader = document.createElement('th')
+        let birthYear = document.createElement('th');
+        let eyeColor = document.createElement('th');
+        let hairColor = document.createElement('th');
+        
+        heightHeader.innerHTML = 'Height'
+        birthYear.innerHTML = 'Birth Year'
+        eyeColor.innerHTML = 'Eye Color'
+        hairColor.innerHTML = 'Hair Color'
+        
+        row.appendChild(heightHeader)
+        row.appendChild(birthYear)
+        row.appendChild(eyeColor)
+        row.appendChild(hairColor)
 
         for (let j=0; j < 1; j++ ) {
-            const column = document.createElement('td');
-            const lukeText = document.createTextNode(`Height: ${data.height}, Birth Year: ${data.birth_year}, Eye Color: ${data.eye_color}, Hair Color: ${data.hair_color}`);
-            column.appendChild(lukeText);
-            row.appendChild(column);
+            let column1 = document.createElement('td');
+            let column2 = document.createElement('td');
+            let column3 = document.createElement('td');
+            let column4 = document.createElement('td');
+            //const lukeText = document.createTextNode(`Height: ${data.height}, Birth Year: ${data.birth_year}, Eye Color: ${data.eye_color}, Hair Color: ${data.hair_color}`);
+            column1.innerHTML = data.height;
+            column2.innerHTML = data.birth_year;
+            column3.innerHTML = data.eye_color;
+            column4.innerHTML = data.hair_color;
+            // j.appendChild(column1);
+            rowSecond.appendChild(column1);
+            rowSecond.appendChild(column2);
+            rowSecond.appendChild(column3);
+            rowSecond.appendChild(column4);
         }
         tableBody.appendChild(row);
+        tableBody.appendChild(rowSecond);
     }
     table.appendChild(tableBody)
 }
